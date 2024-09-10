04c8e01e16ef7c019e283a6abb4a6c927ee49789
Charles Hiribarrondo <96682469+CharlesHiribarrondo@users.noreply.github.com>
2024-09-10 14:29:38 +0200

    Add files via upload

---
+++ Bienvenue.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title('Mes Recettes :open_book:')
+st.header('Objectif du projet :')
+st.write("L'objectif de ce projet est de lister une trentaine de recettes très simples à réaliser, rapides avec des ingrédients facilement trouvables, économiques. Nos recetttes sont en grande majorité végétariennes, car nous souhaitons promouvoir ce type de recettes d'une part, et parce qu'elles sont plus économiques d'autres part. Nous pourrons dans un futur proche intégrer plus de recettes carnivores.")
+st.write("L'idée est de pouvoir planifier facilement ses recettes saines pour la semaine, et éviter des achats de plats préparés mauvais pour notre santé")
+st.write("Notre but avec ce projet est de choisir le nombre de recettes que l'on souhaite, le nombre de personnes par recette choisie, et d'obtenir une liste d'ingrédients classés par rayons que l'on retrouve en supermarché")
+st.write("Bon appétit ! :yum:")
+
+st.write("_NB : pour le moment, le projet est embryonnaire. Il manque une page importante, celle qui donne les instructions des recettes sélectionnées. Cela viendra prochainement. En attendant, nous avons regroupé l'ensemble des recettes dans un PdF [fourni avec le lien qui vous a permis de vous retrouver ici]. Il vous suffira de retrouver vos recettes sélectionnées dans ce document afin de pouvoir déguster vos bons petits plats._")
